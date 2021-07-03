3c3dfe07b497d81fcd41c58f059b648b746e18df
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-03 13:29:30 +0300

    added ; and Hello costumer

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world!;");
-        System.out.println("Hello myself!");
-	System.out.println("Hello GitHub!");
-	System.out.println("Hello GitHub!" + " " + "Update Project");
-	System.out.println("Hello GitHub!" + " " + "Fetch");	   
+        System.out.println("Hello myself!;");
+	System.out.println("Hello GitHub!;");
+	System.out.println("Hello GitHub!" + " " + "Update Project;");
+	System.out.println("Hello GitHub!" + " " + "Fetch;");
+        System.out.println("Hello costumer;");
     }
 }
